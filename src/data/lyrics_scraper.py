17c9855105d3536f5494b500667e239c3cb889d0
Teresa <teresagar181@gmail.com>
2019-08-06 20:11:37 +0200

    Intento de obtener coreano
    Signed-off-by: Teresa <teresagar181@gmail.com>

--- src/data/lyrics_scraper.py
+++ src/data/lyrics_scraper.py
@@ -1,9 +1,16 @@
 from bs4 import BeautifulSoup as bs
+import re
 
 # Con ilyricsbuzz.com
 def get_original_lyrics(raw_html):
-    content = raw_html.find_all('div', {"class": "td-post-content"})[0]
-    return content.get_text()
+    content = raw_html.find('div', {"class": "td-post-content"})
+    content.find('div', {'class': 'code-block code-block-2'}).decompose()
+    content.find('div', {'class': 'code-block code-block-3'}).decompose()
+    b = re.compile('HANGUL(.*)ROMANIZATION', re.S)
+    c = re.compile('HANGUL(.*)MUSIC VIDEO', re.S)
+    final_text = re.search(b, content.get_text()).group(1)
+    final_text2 = re.search(c, final_text).group(1)
+    return final_text2
 
 # Con popgasa.com
 def get_english_lyrics(raw_html):
--- src/models/implementation.py
+++ src/models/implementation.py
@@ -10,7 +10,7 @@ song_data["artist"] = sys.argv[1]
 
 song_data["song_name"] = sys.argv[2]
 
-default_site = "popgasa.com" # esto vendrá de alguna función
+default_site = "ilyricsbuzz.com" # esto vendrá de alguna función
 
 query = song_data["artist"] + " " + song_data["song_name"] + " " + "site:" + default_site
 
@@ -26,6 +26,6 @@ if __name__ == '__main__':
             song_data["url"] = i
             song_data["html"] = tmp_html
             break
-    print(scraper.get_english_lyrics(song_data["html"]))
-    # print(scraper.get_original_lyrics(song_data["html"]))
+    # print(scraper.get_english_lyrics(song_data["html"]))
+    print("Esto es lo que devuelve: \n",scraper.get_original_lyrics(song_data["html"]))
             
