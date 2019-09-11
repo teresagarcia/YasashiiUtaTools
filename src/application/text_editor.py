dc5890fc77afd70645ffcae9e0462d0080fb6708
Teresa <teresagar181@gmail.com>
2019-09-11 17:26:44 +0200

    [Incompleto] Modificar métodos para insertar cŕeditos y etiquetas
    Signed-off-by: Teresa <teresagar181@gmail.com>

--- src/application/text_editor.py
+++ src/application/text_editor.py
@@ -3,7 +3,7 @@ sys.path.append('src')
 from PyQt5.Qt import QApplication, QClipboard
 from PyQt5 import QtCore, QtWidgets
 from editor.editor_setup import MainWindow
-from editor.content_setup import set_title, set_tags, set_credits
+from editor.content_setup import set_title, set_tags, set_credits, get_language_refs
 import utils.file_utils as utils
 
 translation_txt = sys.argv[1]
@@ -17,12 +17,13 @@ if __name__ == '__main__':
     translation = utils.load_txt(translation_txt)
     original_rom = utils.load_txt(original_txt)
     info = utils.load_dict(info_file)
+    language_refs = get_language_refs(info['language'])
 
     editor.title.setText(set_title(info['artist'], info['song_name']))
     editor.translation_board.insertPlainText(translation)
     editor.editing_board.insertPlainText(original_rom)
-    editor.tags.insertPlainText(set_tags(info['artist'], info['song_name'], info['language']))
-    editor.credits.insertPlainText(set_credits(info['original_url'], info['transliteration_url'], info['translation_url']))
+    editor.tags.insertPlainText(set_tags(info['artist'], info['song_name'], language_refs))
+    editor.credits.insertPlainText(set_credits(info['original_url'], info['transliteration_url'], info['translation_url'], language_refs))
     
     editor.show()
     sys.exit( app.exec_() )
--- src/editor/content_setup.py
+++ src/editor/content_setup.py
@@ -1,23 +1,30 @@
+import tldextract
+
 def set_title(artist, song_name):
     title = "[Letra] " + artist + " - " + song_name
     return title
 
-def get_language_tags(language):
+def get_language_refs(language):
     switcher = {
-        'ko': "coreano, romanización",
-        'ja': "japonés, romaji",
-        'hi': "hindi",
-        'ru': "ruso, transliteración",
-        'uk': "ucraniano, transliteración"
+        'ko': {'main_lan': 'coreano', 'translit':'romanización'},
+        'ja': {'main_lan': 'japonés', 'translit':'romaji'},
+        'ru': {'main_lan': 'ruso', 'translit':'transliteración'},
+        'uk': {'main_lan': 'ucraniano', 'translit':'transliteración'}
     }
     return switcher.get(language, "Invalid language")
 
-def set_tags(artist, song_name, language):
+def set_tags(artist, song_name, language_refs):
     song_tags = artist + ", " + song_name
-    language_tags = get_language_tags(language)
     constant_tags = "español, letra"
-    final_tags = song_tags + ", " + language_tags + ", " + constant_tags
+    final_tags = song_tags + ", " + language_refs['main_lan'] + ", " + language_refs['translit'] + ", " + constant_tags
     return final_tags
 
-def set_credits(original_url, transliteration_url, translation_url):
-    return original_url + "\n" + transliteration_url + "\n" + translation_url
+def get_domain(url):
+    ext = tldextract.extract(url)
+    return ext.domain
+
+def set_credits(original_url, transliteration_url, translation_url, language_refs):
+    original_dom = get_domain(original_url)
+    transliteration_dom = get_domain(transliteration_url)
+    translation_dom = get_domain(translation_url)
+    return language_refs['main_lan'] + ": " + original_url + "\n" + language_refs['translit'] + ": " + transliteration_url + "\n" + "Inglés: " + translation_url + "\nTraducción al español: Yasashii Uta"
