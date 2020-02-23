12bc9269f70dbd2585eaca45629328f44eb5fc50
Teresa <teresagar181@gmail.com>
2020-02-23 12:37:46 +0100

    Refactor check_title function at search
    Signed-off-by: Teresa <teresagar181@gmail.com>

--- src/data/search.py
+++ src/data/search.py
@@ -1,7 +1,7 @@
 from googlesearch import search 
 from bs4 import BeautifulSoup as bs
 import requests
-
+import re
 
 # Controlar error HTTP Error 429: Too Many Requests (!!)
 def get_urls(query):
@@ -22,8 +22,18 @@ def get_title(html):
         title = "Sin título"
     return title
 
+def clean_text(*text):
+    test_re = '[^A-Za-z0-9]+'
+    keywords = []
+    [keywords.append(re.sub(test_re, '', word).lower()) for word in text]
+    return keywords
+
+def isTextInTitle(page_title, song_title, artist):
+    return song_title in page_title and artist in page_title
+
 def check_title(title, artist, song_name):
     is_target_page = False
-    if artist.lower() in title.lower() and song_name.lower() in title.lower():
+    title, artist, song_name = clean_text(title, artist, song_name)
+    if isTextInTitle(title, song_name, artist):
         is_target_page = True
     return is_target_page
