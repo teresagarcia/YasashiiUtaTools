7e665ac7e0ec830900e0d90068f8fb574df11e48
Teresa <teresagar181@gmail.com>
2020-03-01 16:40:49 +0100

    Refactor, add exception
    Signed-off-by: Teresa <teresagar181@gmail.com>

--- src/application/lyrics_extraction.py
+++ src/application/lyrics_extraction.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('src') 
 from constants import constants
-import search.search as search
+from search.search import check_title, get_title, get_html, get_urls
 from data.song import Song
 import scrapers.lyrics_scraper as scraper
 import scrapers.kpop_post2015 as kpop2015
@@ -10,38 +10,51 @@ import json
 output_file = constants.BASE_INFO
 song = Song()
 
-def get_page(urls):
+def __get_page(urls):
+    url = ""
+    html = ""
     for i in urls:
-        tmp_html = search.get_html(i)
-        title = search.get_title(tmp_html)
-        print(title)
-        if search.check_title(title, song.artist, song.song_name ):
-            url = i 
-            html = tmp_html
-            break 
-        # ¿Y si no lo encuentra?
-    return html, url
+            tmp_html = get_html(i)
+            title = get_title(tmp_html)
+            if check_title(title, song.artist, song.song_name):
+                url = i 
+                html = tmp_html
+                break 
+    if url == "":
+        raise ValueError("No se encontró la página")    
+    return url, html
 
 def set_search_data():
     print("Dime el artista:")
-    song.artist = input()
+    artist = input()
     print("Y la canción:")
-    song.song_name = input()
+    song_name = input()
+    return artist, song_name
+
+def __get_page_data(queries):
+    page_data = {}
+    page_data["original_url"], page_data["original_html"] = __get_page(get_urls(queries["original_rom_lyrics"]))
+    page_data["transliteration_url"], page_data["transliteration_html"] = __get_page(get_urls(queries["original_rom_lyrics"]))
+    page_data["translation_url"], page_data["translation_html"] = __get_page(get_urls(queries["english_lyrics"]))
+    return page_data
 
 def extract_lyrics():
-    set_search_data()
+    song.artist, song.song_name = set_search_data()
 
     queries = kpop2015.get_queries(song.artist, song.song_name)
     
-    original_html, song.original_url = get_page(search.get_urls(queries["original_rom_lyrics"]))
-    rom_html, song.transliteration_url = get_page(search.get_urls(queries["original_rom_lyrics"]))
-    english_html, song.translation_url = get_page(search.get_urls(queries["english_lyrics"]))
+    page_data = __get_page_data(queries)
+
+    song.original_url = page_data["original_url"],
+    song.transliteration_url = page_data["transliteration_url"]
+    song.translation_url = page_data["translation_url"]
+    song.original = scraper.get_original_lyrics(page_data["original_html"])
+    song.transliteration = scraper.get_romanization(page_data["transliteration_html"])
+    song.translation = scraper.get_english_lyrics(page_data["translation_html"] )
     
-    song.original = scraper.get_original_lyrics(original_html)
-    song.transliteration = scraper.get_romanization(rom_html)
-    song.translation = scraper.get_english_lyrics(english_html)
     with open(output_file, 'w') as outfile:
         json.dump(song.toJSON(), outfile)
+    
     print("Éxito~")
 
 if __name__ == '__main__':
